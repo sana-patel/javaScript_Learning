d56407ddd2c29d197e2b96cc63f40f4e922a1393
sana-patel <sana.patels@gmail.com>
2022-10-04 08:36:56 +0530

    javaScript_intro
    javaScript_intro

---
+++ demo.js
@@ -0,0 +1,10 @@
+// This is my first program
+
+
+console.log("Hello")
+console.log("My name is sana")
+console.log("My name is Riza")
+console.info("This is info")  //this is information
+//warn means warning
+console.warn("I am not that bad guy")
+console.error("opps failed to execute")
