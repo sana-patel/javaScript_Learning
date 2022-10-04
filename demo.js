752c9e9a347b78035a5e404923bb0f504418b85b
sana-patel <sana.patels@gmail.com>
2022-10-04 13:19:12 +0530

    'First Program execution in JS'
    'First Program execution in JS'

--- demo.js
+++ demo.js
@@ -1,10 +1,18 @@
 // This is my first program
 
 
-console.log("Hello")
-console.log("My name is sana")
-console.log("My name is Riza")
-console.info("This is info")  //this is information
-//warn means warning
-console.warn("I am not that bad guy")
-console.error("opps failed to execute")
+// console.log("Hello")
+// console.log("My name is sana")
+// console.log("My name is Riza")
+// console.info("This is info")  //this is information
+// //warn means warning
+// console.warn("I am not that bad guy")
+// console.error("opps failed to execute")
+
+
+console.log("MY full name is - Sana sohel Patel");
+console.log("My Hobbies are - 1.cooking 2.gardening 3.reading");
+console.info("Personal Strength- 1.Creativity 2.Versatility 3.Dedication")
+console.warn("My weakness- 1.self-criticism 2.Sensitive 3.Emotional")       //weakness
+console.log(30+50);
+console.log(49*479);
